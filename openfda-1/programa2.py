7dd798ee8f07adc99acbeb91b7044f195efd7ae1
carlagg <cg.gallego18@gmail.com>
2018-04-03 17:53:31 +0200

    1

--- openfda-1/programa2.py
+++ openfda-1/programa2.py
@@ -7,20 +7,18 @@ headers = {'User-Agent': 'http-client'}
 
 llamar_servidor = http.client.HTTPSConnection("api.fda.gov")
 
+#ponemos el limite en 10 porque queremos saber el ID de los primeros 1os medicamentos. Esto se incluye dentro del recurso.
 llamar_servidor.request("GET", "/drug/label.json?limit=10", None, headers)
-#ponemos el limite en 10 porque es lo que pide el ejercicio (imprime datos de los 10 primeros medicamentos).
 
 respuesta = llamar_servidor.getresponse()
 contenido_labeĺ = respuesta.read().decode("utf-8")
 llamar_servidor.close() #fianlizmos la conexón con el servidor.
-
-
 label_estructurado = json.loads(contenido_labeĺ)
 
-#creamos un bucle que recorra toda la longitud del contenido de la respuesta pero ya estructurado.
-#que se centre solo en los datos dentro de 'results'.
+#creamos un bucle que recorra toda la longitud del contenido de la respuesta pero ya estructurada.
+#sólo en los datos dentro de 'results'.
 for i in range (len (label_estructurado['results'])):
     informacion_medicamento=label_estructurado['results'][i]
 
-    #dentro de la info hay una lista con el id.
+    #dentro de la info de 'results' hay una lista con el id.
     print ('ID: ',informacion_medicamento['id'])
