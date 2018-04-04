b7e8257a9298e533db599d5f3b76baa2d2da9552
carlagg <cg.gallego18@gmail.com>
2018-04-04 11:51:15 +0200

    1

--- openfda-2/programa3.py
+++ openfda-2/programa3.py
@@ -1,27 +1,32 @@
-#recuperamos el códifo usado en 'programa1.py' y  'programa2.py'.
-
+#recuperamos parte del código usado en 'programa1.py' y  'programa2.py'.
 import http.client
 import json
 
-
 headers = {'User-Agent': 'http-client'}
 
+#entablamos conexión con el servidor.
 llamar_servidor = http.client.HTTPSConnection("api.fda.gov")
-#el programa puede coger como máximo 100 medicamentos que tengan 'aspirin' entre sus componentes.
-#imprimirá por pantalla los 100 primeros que aparezcan.
+num_skip = 0
+while True: #bucle infinito.
 
-llamar_servidor.request("GET", '/drug/label.json?limit=100&search=substance_name:"ASPIRIN"', None, headers)
-respuesta = llamar_servidor.getresponse()
-contenido_label = respuesta.read().decode("utf-8")
+    #imprimirá por pantalla los 100 primeros que aparezcan(limit).
+    #usamos la función skip.
+    llamar_servidor.request("GET", '/drug/label.json?limit=100&skip='+ str(num_skip)+'&search=substance_name:"ASPIRIN"', None, headers)
+    respuesta = llamar_servidor.getresponse()
+    contenido_label = respuesta.read().decode("utf-8")
+
+    label_estructurado = json.loads(contenido_label)
+
+    #imprimimos 'ID' y 'Fabricante' para cada uno de los medicamentos que recorre el bucle.
+    for i in range (len (label_estructurado['results'])):
+        informacion_medicamento=label_estructurado['results'][i]
+        print (' ID: ',informacion_medicamento['id'],'\n','Fabricante: ', informacion_medicamento['openfda']['manufacturer_name'])
+
+    #si de los 100 medicamentos, no todos tienen 'ASPIRIN' el bucle se rompe con un 'break'.
+    if len(label_estructurado['results'])<100:
+            break
+
+    #si los 100 medicamentos tienen 'ASPIRIN' entonces se saltan los 100 primeros y se continua el bucle.
+    num_skip =+100
+
 llamar_servidor.close() #finalizamos la conexión con el servidor.
-
-label_estructurado = json.loads(contenido_label)
-
-
-#creamos un bucle que recorra 'results' dentro de la información que nos dan pero estructurada dentro de la variable 'label_estructurado'..
-#en la variable 'informacion_medicamento' metemos cada uno de los datos de 'results'.
-#imprimimos 'ID' y 'Fabricante' para cada uno de los meddicamentos que recorre e bucle.
-for i in range (len (label_estructurado['results'])):
-    informacion_medicamento=label_estructurado['results'][i]
-
-    print (' ID: ',informacion_medicamento['id'],"\n",'Fabricante: ', informacion_medicamento['openfda']['manufacturer_name'])
